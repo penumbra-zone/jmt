83256d90baaf6f6de1cebefbc194b7df5a1c494b
aubrey <aubrey@penumbra.zone>
2021-12-14 15:32:56 -0800

    importing dependencies
    Co-authored-by: plaidfinch <finch@plaidfinch.net>

---
+++ src/types/proof/mod.rs
@@ -0,0 +1,82 @@
+// Copyright (c) The Diem Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+pub mod definition;
+#[cfg(any(test, feature = "fuzzing"))]
+pub mod proptest_proof;
+
+use crate::hash::{
+    CryptoHash, CryptoHasher, EventAccumulatorHasher, HashValue, SparseMerkleInternalHasher,
+    SparseMerkleLeafNodeHasher, TestOnlyHasher, TransactionAccumulatorHasher,
+};
+use diem_crypto_derive::CryptoHasher;
+#[cfg(any(test, feature = "fuzzing"))]
+use proptest_derive::Arbitrary;
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+
+pub use self::definition::{SparseMerkleProof, SparseMerkleRangeProof};
+
+pub struct MerkleTreeInternalNode<H> {
+    left_child: HashValue,
+    right_child: HashValue,
+    hasher: PhantomData<H>,
+}
+
+impl<H: CryptoHasher> MerkleTreeInternalNode<H> {
+    pub fn new(left_child: HashValue, right_child: HashValue) -> Self {
+        Self {
+            left_child,
+            right_child,
+            hasher: PhantomData,
+        }
+    }s
+}
+
+impl<H: CryptoHasher> CryptoHash for MerkleTreeInternalNode<H> {
+    type Hasher = H;
+
+    fn hash(&self) -> HashValue {
+        let mut state = Self::Hasher::default();
+        state.update(self.left_child.as_ref());
+        state.update(self.right_child.as_ref());
+        state.finish()
+    }
+}
+
+pub type SparseMerkleInternalNode = MerkleTreeInternalNode<SparseMerkleInternalHasher>;
+pub type TransactionAccumulatorInternalNode = MerkleTreeInternalNode<TransactionAccumulatorHasher>;
+pub type EventAccumulatorInternalNode = MerkleTreeInternalNode<EventAccumulatorHasher>;
+pub type TestAccumulatorInternalNode = MerkleTreeInternalNode<TestOnlyHasher>;
+
+#[derive(Clone, Copy, CryptoHasher, Debug, Eq, PartialEq, Serialize, Deserialize)]
+#[cfg_attr(any(test, feature = "fuzzing"), derive(Arbitrary))]
+pub struct SparseMerkleLeafNode {
+    key: HashValue,
+    value_hash: HashValue,
+}
+
+impl SparseMerkleLeafNode {
+    pub fn new(key: HashValue, value_hash: HashValue) -> Self {
+        SparseMerkleLeafNode { key, value_hash }
+    }
+
+    pub fn key(&self) -> HashValue {
+        self.key
+    }
+
+    pub fn value_hash(&self) -> HashValue {
+        self.value_hash
+    }
+}
+
+impl CryptoHash for SparseMerkleLeafNode {
+    type Hasher = SparseMerkleLeafNodeHasher;
+
+    fn hash(&self) -> HashValue {
+        let mut state = Self::Hasher::default();
+        state.update(self.key.as_ref());
+        state.update(self.value_hash.as_ref());
+        state.finish()
+    }
+}
